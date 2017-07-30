75c8486b398aa7bdf2943bbcd649fc0de5aee9cf
webwerks <webwerks@webwerkss-mukesh-macbook-pro.local>
2017-07-30 10:49:00 +0530

    Initial Commit

---
+++ PageViewControllerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PageViewControllerDemo
+//
+//  Created by webwerks on 30/07/17.
+//  Copyright © 2017 smart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
